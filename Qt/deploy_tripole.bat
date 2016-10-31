f69a15eb3d7d0d765c02251591e06cc40f0828d4
Norton Allen <allen@huarp.harvard.edu>
2016-10-31 15:50:56 -0400

    Include default config file

--- Qt/deploy_tripole.bat
+++ Qt/deploy_tripole.bat
@@ -1,3 +1,4 @@
 SET VCINSTALLDIR=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC
 copy build-tripole-Desktop_Qt_5_7_0_MSVC2013_64bit-Debug\debug\tripole.exe deploy
+copy tripole_init.dat deploy
 windeployqt -serialport deploy\tripole.exe
